fef0e8e854ee66d927ec7887553f8c2829d407b6
V <V@V-2.local>
2026-08-12 18:12:00 +0800

    fix: refine status bar cat icon

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -44,9 +44,9 @@ cat > "$contents_dir/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.11.1</string>
+    <string>1.11.2</string>
     <key>CFBundleVersion</key>
-    <string>16</string>
+    <string>17</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>LSUIElement</key>
