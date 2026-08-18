4fc6437f71f1cdf53afe5298754ea5cfb2531ad5
V <vincent.liuc@outlook.com>
2026-08-18 09:51:42 +0800

    release: prepare QuotaBar 1.11.14

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -10,8 +10,8 @@ contents_dir="$bundle_dir/Contents"
 output_zip="$project_dir/outputs/QuotaBar-macOS.zip"
 source_zip="$project_dir/outputs/QuotaBar-source.zip"
 architectures=(--arch arm64 --arch x86_64)
-app_version="${QUOTABAR_VERSION:-1.11.13}"
-app_build="${QUOTABAR_BUILD:-28}"
+app_version="${QUOTABAR_VERSION:-1.11.14}"
+app_build="${QUOTABAR_BUILD:-29}"
 
 cd "$project_dir"
 zsh scripts/build-icon.sh
