8250b66bb9df16c4f3df7a1ac4eb37acd361f0ad
V <vincent.liuc@outlook.com>
2026-08-31 16:45:35 +0800

    chore: bump version to 1.11.23

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -10,8 +10,8 @@ contents_dir="$bundle_dir/Contents"
 output_zip="$project_dir/outputs/QuotaBar-macOS.zip"
 source_zip="$project_dir/outputs/QuotaBar-source.zip"
 architectures=(--arch arm64 --arch x86_64)
-app_version="${QUOTABAR_VERSION:-1.11.22}"
-app_build="${QUOTABAR_BUILD:-37}"
+app_version="${QUOTABAR_VERSION:-1.11.23}"
+app_build="${QUOTABAR_BUILD:-38}"
 
 cd "$project_dir"
 zsh scripts/build-icon.sh
